cdb43fac5eb249a66733b9315cf14ef9353e2127
Devansh Gupta <devansh_gupta91@hotmail.com>
2014-02-22 01:19:22 -0800

    Tweak bubbles

--- src/com/dream/juju/BubbleView.java
+++ src/com/dream/juju/BubbleView.java
@@ -15,19 +15,19 @@ public class BubbleView extends View {
 		int color;
 	}
 
-	public static final int NUM_CIRCLES = 20;
+	public static final int NUM_CIRCLES = 40;
 	public static final float MIN_RADIUS = 0.05f;
 	public static final float MAX_RADIUS = 0.25f;
 	public static final float MIN_SPEED = 0.05f;
 	public static final float MAX_SPEED = 0.15f;
 	public static final float MAX_BELOW = 1000.0f;
-	
+
 	public static final float SPEED_DIFF = MAX_SPEED - MIN_SPEED;
 	public static final float RADIUS_DIFF = MAX_RADIUS - MIN_RADIUS;
 
 	public static final Circle[] CIRCLES = new Circle[NUM_CIRCLES];
 	public static final Paint PAINT = new Paint();
-	
+
 	Runnable animation = new Runnable() {
 		@Override
 		public void run() {
@@ -38,7 +38,7 @@ public class BubbleView extends View {
 			postDelayed(this, 50);
 		}
 	};
-	
+
 	boolean inited = false;
 
 	public BubbleView(Context context) {
@@ -52,7 +52,7 @@ public class BubbleView extends View {
 	public BubbleView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 	}
-	
+
 	public void init() {
 		for (int i = 0; i < NUM_CIRCLES; i++) {
 			CIRCLES[i] = new Circle();
@@ -63,26 +63,28 @@ public class BubbleView extends View {
 
 	public void initRandomly(Circle c) {
 		float halfWidth = getWidth() / 2.0f;
-		c.x = (float) ((2 * Math.random() - 1) * halfWidth * 0.75f + halfWidth);
-		c.y = getHeight() + (float) (Math.random() * MAX_BELOW);
-		c.speed = (float) (Math.random() * SPEED_DIFF + MIN_SPEED);
-		c.color = Color.argb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
 		c.radius = (float) (halfWidth * Math.random() * RADIUS_DIFF + MIN_RADIUS);
+		c.x = (float) ((2 * Math.random() - 1) * halfWidth * MAX_RADIUS + halfWidth);
+		c.y = getHeight() + (float) (Math.random() * MAX_BELOW) + c.radius;
+		c.speed = -(float) (Math.random() * SPEED_DIFF + MIN_SPEED);
+		c.color = Color.argb((int) (Math.random() * 255),
+				(int) (Math.random() * 255), (int) (Math.random() * 255),
+				(int) (Math.random() * 255));
 	}
-	
+
 	public void updateCircle(Circle c, float deltaTime) {
-		c.y -= c.speed * deltaTime;
+		c.y += c.speed * deltaTime;
 		if (c.y + c.radius <= 0) {
 			initRandomly(c);
 		} else {
 			c.x += (2 * Math.random() - 1) * getWidth() * 0.001f;
 		}
 	}
-	
+
 	public void startAnimation() {
 		post(animation);
 	}
-	
+
 	public void stopAnimation() {
 		removeCallbacks(animation);
 		setVisibility(View.GONE);
