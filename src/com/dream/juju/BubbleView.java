a8dfcdcc4a57a4f1e26e70752f529d3779a2a13e
Devansh Gupta <devansh_gupta91@hotmail.com>
2014-02-22 14:26:26 -0800

    Animate in Circle Groups

--- src/com/dream/juju/BubbleView.java
+++ src/com/dream/juju/BubbleView.java
@@ -1,10 +1,7 @@
 package com.dream.juju;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -13,16 +10,12 @@ public class BubbleView extends View {
 	private static class Circle {
 		float x, y;
 		float radius;
-		float speed;
 		int color;
-		CircleGroup group;
-		int colorIndex;
 	}
 
 	private static class CircleGroup {
 		Circle[] circles;
-		int order;
-		ArrayList<Integer> colors;
+		float y;
 	}
 
 	public static final int TIME_STEP = 50;
@@ -30,7 +23,7 @@ public class BubbleView extends View {
 	public static final int NUM_CIRCLES_PER_GROUP = 20;
 	public static final float MIN_RADIUS = 50f;
 	public static final float MAX_RADIUS = 150f;
-	public static final float SPEED = 0.1f;
+	public static final float SPEED = -0.1f;
 	public static final float GROUP_HEIGHT = 1000.0f;
 
 	public static final float RADIUS_DIFF = MAX_RADIUS - MIN_RADIUS;
@@ -42,11 +35,7 @@ public class BubbleView extends View {
 		@Override
 		public void run() {
 			for (int i = 0; i < NUM_CIRCLE_GROUPS; i++) {
-				Circle[] circles = CIRCLE_GROUPS[i].circles;
-				int len = circles.length;
-				for (int j = 0; j < len; j++) {
-					updateCircle(circles[j], TIME_STEP);
-				}
+				updateGroup(CIRCLE_GROUPS[i], TIME_STEP);
 			}
 			invalidate();
 			postDelayed(this, TIME_STEP);
@@ -54,6 +43,8 @@ public class BubbleView extends View {
 	};
 
 	boolean inited = false;
+
+	CircleGroup bottomMostGroup = null;
 
 	public BubbleView(Context context) {
 		super(context);
@@ -69,64 +60,53 @@ public class BubbleView extends View {
 
 	public void init() {
 		CircleGroup g;
+		float height = getHeight();
 		for (int i = 0; i < NUM_CIRCLE_GROUPS; i++) {
 			g = new CircleGroup();
-			g.order = i;
 			g.circles = new Circle[NUM_CIRCLES_PER_GROUP];
-			g.colors = new ArrayList<Integer>(5);
-			initCircleGroup(g);
+			resetGroup(g, height + i * GROUP_HEIGHT);
 			CIRCLE_GROUPS[i] = g;
 		}
+		bottomMostGroup = CIRCLE_GROUPS[NUM_CIRCLE_GROUPS - 1];
 		inited = true;
 	}
 
-	public void initCircleGroup(CircleGroup group) {
+	public void resetGroup(CircleGroup g, float y) {
+		g.y = y;
+		int red = (int) (Math.random() * 255);
+		int green = (int) (Math.random() * 255);
+		int blue = (int) (Math.random() * 255);
+		int color = 0x00000000 | (red << 16) | (green << 8) | blue;
+
 		float halfWidth = getWidth() / 2.0f;
-		Circle[] circles = group.circles;
-		int length = circles.length;
-
-		float startY = getHeight() + group.order * GROUP_HEIGHT;
-
-		Circle c;
-		for (int i = 0; i < length; i++) {
-			c = new Circle();
+		Circle[] circles = g.circles;
+		int len = circles.length;
+		for (int i = 0; i < len; i++) {
+			Circle c = circles[i];
+			if (c == null) {
+				circles[i] = new Circle();
+				c = circles[i];
+			}
+			c.color = color
+					| ((((int) (Math.random() * 255)) << 24) & 0xFF000000);
 			c.radius = (float) (Math.random() * RADIUS_DIFF + MIN_RADIUS);
 			c.x = (float) ((2 * Math.random() - 1) * halfWidth * 0.1f + halfWidth);
-			c.y = startY + (float) (Math.random() * GROUP_HEIGHT);
-			c.speed = -SPEED;
-			c.group = group;
-			assignNewColor(c);
-			circles[i] = c;
+			c.y = (float) (Math.random() * GROUP_HEIGHT);
 		}
 	}
 
-	public void assignNewColor(Circle c) {
-		CircleGroup g = c.group;
-		if (c.colorIndex >= g.colors.size()) {
-			int red = (int) (Math.random() * 255);
-			int green = (int) (Math.random() * 255);
-			int blue = (int) (Math.random() * 255);
-			g.colors.add(0x00000000 | (red << 16) | (green << 8) | blue);
-		}
-		int color = g.colors.get(c.colorIndex);
-		c.color = color | ((((int) (Math.random() * 255)) << 24) & 0xFF000000);
-		c.colorIndex++;
-	}
-
-	public void resetRandomly(Circle c) {
-		float halfWidth = getWidth() / 2.0f;
-		c.radius = (float) (Math.random() * RADIUS_DIFF + MIN_RADIUS);
-		c.x = (float) ((2 * Math.random() - 1) * halfWidth * 0.1f + halfWidth);
-		c.y = getHeight() + (float) (Math.random() * GROUP_HEIGHT);
-		assignNewColor(c);
-	}
-
-	public void updateCircle(Circle c, float deltaTime) {
-		c.y += c.speed * deltaTime;
-		if (c.y + c.radius <= 0) {
-			resetRandomly(c);
+	public void updateGroup(CircleGroup g, float deltaTime) {
+		g.y += SPEED * deltaTime;
+		if (g.y + GROUP_HEIGHT <= 0) {
+			resetGroup(g, bottomMostGroup.y + GROUP_HEIGHT);
+			bottomMostGroup = g;
 		} else {
-			c.x += (2 * Math.random() - 1) * getWidth() * 0.001f;
+			int width = getWidth();
+			Circle[] circles = g.circles;
+			int len = circles.length;
+			for (int i = 0; i < len; i++) {
+				circles[i].x += (2 * Math.random() - 1) * width * 0.001f;
+			}
 		}
 	}
 
@@ -144,17 +124,20 @@ public class BubbleView extends View {
 			return;
 		}
 		Circle c;
+		CircleGroup g;
 		int height = getHeight();
 		for (int i = 0; i < NUM_CIRCLE_GROUPS; i++) {
-			Circle[] circles = CIRCLE_GROUPS[i].circles;
+			g = CIRCLE_GROUPS[i];
+			Circle[] circles = g.circles;
 			int len = circles.length;
 			for (int j = 0; j < len; j++) {
 				c = circles[j];
-				if (c.y - c.radius >= height) {
+				float y = g.y + c.y;
+				if (y - c.radius >= height) {
 					continue;
 				}
 				PAINT.setColor(c.color);
-				canvas.drawCircle(c.x, c.y, c.radius, PAINT);
+				canvas.drawCircle(c.x, y, c.radius, PAINT);
 			}
 		}
 	}
