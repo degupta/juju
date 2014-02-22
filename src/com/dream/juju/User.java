e23aa58a84e81be19d9ff1c2cc6f3ce295effc1e
Devansh Gupta <devansh_gupta91@hotmail.com>
2014-02-22 12:50:56 -0800

    Fb login

--- src/com/dream/juju/User.java
+++ src/com/dream/juju/User.java
@@ -1,9 +1,15 @@
 package com.dream.juju;
 
+import com.facebook.model.GraphUser;
 import com.parse.ParseUser;
 
 public class User {
 	public ParseUser parseUser;
+	public GraphUser graphUser;
+
 	public User() {
+	}
+
+	public void getImageUrl() {
 	}
 }
