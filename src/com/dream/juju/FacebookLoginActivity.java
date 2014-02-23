8a9e185ac9ea5953b2716d4d2eeaa3498956dcdd
Devansh Gupta <devansh_gupta91@hotmail.com>
2014-02-22 21:41:24 -0800

    Tweak login screen

--- src/com/dream/juju/FacebookLoginActivity.java
+++ src/com/dream/juju/FacebookLoginActivity.java
@@ -14,7 +14,6 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import com.facebook.Request;
